b245326f9875dfa2c736c3b03dac87a5950ef8ff
tehstoni <stevenharrisskater66@hotmail.com>
2024-03-17 11:08:44 -0400

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -6,27 +6,22 @@ def read_input_file(input_file):
         input_data = file.read().replace('"', '').replace(';', '').replace('\n', '').replace(' ', '').replace('0x', '\\x').replace(',', '').replace('b', '').replace("'", '')
     return input_data
 
-
-def write_output_file(output_file, hex_data):
-    with open(output_file, 'w') as file:
-        file.write(hex_data)
-
+def write_output_file(output_file, shellcode):
+    shellcode_bytes = bytes(shellcode, encoding='latin1')
+    with open(output_file, "wb") as file:
+        file.write(shellcode_bytes)
 
 def main():
     parser = argparse.ArgumentParser(description='Process input and output files.')
 
-    parser.add_argument('-i', '--input', help='Input file')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-i', '--input', help='Input file', required=True)
+    parser.add_argument('-o', '--output', help='Output file', required=True)
 
     args = parser.parse_args()
 
-    input_file = args.input
-    output_file = args.output
-
-    input_data = read_input_file(input_file)
-    print("input data:" + input_data + '\n')
-    write_output_file(output_file, input_data)
+    shellcode = read_input_file(args.input)
     
+    write_output_file(args.output, shellcode)
 
 if __name__ == "__main__":
     main()
