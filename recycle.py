d40dca7e4d7f305215f5339972e8519a96059216
tehstoni <stevenharrisskater66@hotmail.com>
2024-03-17 11:29:55 -0400

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -7,22 +7,17 @@ def read_input_file(input_file):
     return input_data
 
 def write_output_file(output_file, shellcode):
-    shellcode_bytes = bytes(shellcode, encoding='latin1')
+    shellcode_bytes = bytes(shellcode, 'utf-8')
     with open(output_file, "wb") as file:
         file.write(shellcode_bytes)
 
 def main():
     parser = argparse.ArgumentParser(description='Process input and output files.')
-
     parser.add_argument('-i', '--input', help='Input file', required=True)
     parser.add_argument('-o', '--output', help='Output file', required=True)
-
     args = parser.parse_args()
-
     shellcode = read_input_file(args.input)
-    
     print(shellcode[:100] + "...")
-
     write_output_file(args.output, shellcode)
 
 if __name__ == "__main__":
