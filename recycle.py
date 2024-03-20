77123a107e6c5929218ca47723e11c414e6ed4ae
tehstoni <stevenharrisskater66@hotmail.com>
2024-03-19 20:28:46 -0400

    made less potato

--- recycle.py
+++ recycle.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python3
 import argparse
 
-def read_input_file(input_file):
-    with open(input_file, 'r') as file:
-        input_data = file.read().replace('"', '').replace(';', '').replace('\n', '').replace(' ', '').replace('0x', '\\x').replace(',', '').replace('b', '').replace("'", '')
-    return input_data
-
-def write_output_file(output_file, shellcode):
-    shellcode_bytes = bytes(shellcode, 'utf-8')
-    with open(output_file, "wb") as file:
-        file.write(shellcode_bytes)
+def convert_string_to_bytes(shellcode_string):
+    # First, ensure we remove all occurrences of "\x" which are not part of the hex value
+    # Then, filter out any non-hexadecimal characters that may have sneaked into our incantation
+    hex_string = ''.join(shellcode_string.split("\\x")[1:])
+    hex_string = ''.join(filter(lambda x: x in '0123456789abcdefABCDEF', hex_string))
+    # Now convert what remains into raw bytes
+    return bytes.fromhex(hex_string)
 
 def main():
-    parser = argparse.ArgumentParser(description='Process input and output files.')
-    parser.add_argument('-i', '--input', help='Input file', required=True)
-    parser.add_argument('-o', '--output', help='Output file', required=True)
+    parser = argparse.ArgumentParser(description="This script reads shellcode from an input file in a textual format and writes the raw bytes to an output file.")
+    parser.add_argument("-i", "--input", required=True, help="Input file containing shellcode in a string format.", type=str)
+    parser.add_argument("-o", "--output", required=True, help="Output file to write the raw shellcode bytes to.", type=str)
     args = parser.parse_args()
-    shellcode = read_input_file(args.input)
-    print(shellcode[:100] + "...")
-    write_output_file(args.output, shellcode)
+
+    # Read the shellcode from the input file
+    with open(args.input, "r") as file:
+        shellcode_string = file.read()
+
+    # Convert the string representation of the shellcode into raw bytes
+    shellcode_bytes = convert_string_to_bytes(shellcode_string)
+
+    # Write the raw bytes to the specified output file
+    with open(args.output, "wb") as file:
+        file.write(shellcode_bytes)
+    
+    print(f"Shellcode has been successfully written to {args.output}")
 
 if __name__ == "__main__":
     main()
